581ccf4731ea782e97573244884c881e4b6f7fd3
jonas <jonas.peixoto.087@gmail.com>
2023-11-11 20:15:08 -0300

    Conectando com banco de dados MYSQL

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,17 @@
 from flask import Flask 
 
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+USERNAME = 'root'
+PASSWORD = '12345678'
+SERVER = 'localhost'
+DB = 'bd'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}'
+
+db = SQLAlchemy(app)
 
 from .views import cliente_views
 
